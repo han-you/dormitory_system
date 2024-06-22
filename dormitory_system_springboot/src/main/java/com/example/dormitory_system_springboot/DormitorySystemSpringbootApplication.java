1b4850e9f3042018e1bdcf4ea626ed47bf77e5aa
han-you <2420380374@qq.com>
2024-06-22 10:57:01 +0800

    create the project

---
+++ dormitory_system_springboot/src/main/java/com/example/dormitory_system_springboot/DormitorySystemSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.dormitory_system_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormitorySystemSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DormitorySystemSpringbootApplication.class, args);
+    }
+
+}
---
+++ dormitory_system_springboot/src/test/java/com/example/dormitory_system_springboot/DormitorySystemSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dormitory_system_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DormitorySystemSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
